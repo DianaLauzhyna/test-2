3d04148021f22ece143b85aa2fe82963b9c85bd6
Diana Lauzhyna <dlauzhyna@gmail.com>
2022-09-20 11:57:23 +0300

    test

--- src/components/budjet_files/app.js
+++ src/components/budjet_files/app.js
@@ -2,13 +2,15 @@
 import { Component } from 'react';
 import Balance from '../balance/balance.js'
 
+let id = 0;
 
 class Appp extends Component {
     constructor ()
         {
             super();
             this.state ={
-                balance: 0
+                balance: 0,
+                transactions: []
             }
             this.onIncrease = this.onIncrease.bind(this);
             //this.onDecrease = this.onDecrease.bind(this);
@@ -41,24 +43,44 @@ componentWillUnmount(){
     debugger
 } */
     onIncrease() {
-        this.setState({balance: this.state.balance +1})
+        this.setState((state)=>({ 
+            balance: state.balance +1,
+            transactions: [{
+                label:'increase',
+                value: 1,
+            id:++id}, ...state.transactions]
+        }))
     }
      
     onDecrease = ()=>{
-        this.setState({balance: this.state.balance -1})
+        this.setState((state)=>({ 
+            balance: state.balance -1,
+            transactions: [{
+                label:'decrease',
+                value: -1,
+            id:++id}, ...state.transactions]
+        }))
     }
 
     render () {
         console.log('render')
      return (
       <div>
-       <Balance balance={this.state.balance}>
-        Balance
-       </Balance>
+       <Balance balance={this.state.balance}/>
        <button onClick={this.onIncrease}> Add money</button>
        <button onClick={this.onDecrease}> Remove money</button>
+       <hr/> 
+       {
+this.state.transactions.map((transaction) =>(
+    <div key={transaction.id}>
+        Label: {transaction.label}
+        <p>Value: {transaction.value}</p>
+        <br/>
+    </div>
+))        
+       }
       </div>
-    )   
+     ) 
     } 
   };
 
